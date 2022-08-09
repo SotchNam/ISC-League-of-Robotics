c65f8dfb35f94fca1f9bb263661255d32eddcca6
SotchNam <sotchnam@gmail.com>
2022-08-09 09:39:53 +0100

    removed colordetect.py since it's not needed, added comments in colorprint.py, added farouk's HSV_Treshholder.py

--- roboticleague/color detection/colorprint.py
+++ roboticleague/color detection/colorprint.py
@@ -12,9 +12,13 @@ def scanColor(imageFrame):
     imageFrame = imageFrame[y1:y2,x1:x2]
     #bgr to hsv
     hsvFrame = cv2.cvtColor(imageFrame, cv2.COLOR_BGR2HSV)
+#getting avg color from frame, might consider using dominant color
     avg_color_per_row = np.average(hsvFrame, axis=0) 
     avg_color = np.average(avg_color_per_row, axis=0)
 
+#color ranges: hsv (180,255,255)max
+#red has a part in beginning and the end of hue spectrum so two ranges are needed
+#use HSV_Treshhold.py to get calibrated values
     red_lower1 = np.array([0, 87, 111], np.uint8)
     red_upper1 = np.array([25, 255, 255], np.uint8) 
 
@@ -27,6 +31,8 @@ def scanColor(imageFrame):
     blue_lower = np.array([94, 80, 2], np.uint8)
     blue_upper = np.array([120, 255, 255], np.uint8)
 
+#test the avg color with each color range
+
     if  ((np.less_equal(avg_color,red_upper1)).all() and (np.greater_equal(avg_color,red_lower1)).all()) or ((np.less_equal(avg_color,red_upper)).all() and (np.greater_equal(avg_color,red_lower)).all()):
             return 'R'
 
@@ -36,14 +42,13 @@ def scanColor(imageFrame):
     elif (np.less_equal(avg_color,blue_upper)).all() and (np.greater_equal(avg_color,blue_lower)).all():
             return 'B'
 
-
-    # Program Termination
+#code that runs when running this file directly
 if __name__ == '__main__':
     webcam = cv2.VideoCapture(0)
     while(True):
         _, imageFrame = webcam.read()
         print(scanColor(imageFrame))
-        cv2.imshow("Multiple Color Detection in Real-TIme", imageFrame)
+        cv2.imshow("test", imageFrame)
         if cv2.waitKey(10) & 0xFF == ord('q'):
                 webcam.release()
                 cv2.destroyAllWindows()
