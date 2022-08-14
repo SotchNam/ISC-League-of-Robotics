1e6d4326de62e04d297879b3db129095e392bdce
SotchNam <sotchnam@gmail.com>
2022-08-14 23:30:03 +0100

    Added cameraInput.py. Modified all modules to be in class form. Tested camera and color but not crossing or line. cameraInput.py might be changed to adapt to raspi cam.

--- roboticleague/crossing.py
+++ roboticleague/crossing.py
@@ -2,55 +2,66 @@
 import RPi.GPIO as GPIO
 import time
 
-# GPIO Mode (BOARD / BCM)
-GPIO.setmode(GPIO.BCM)
+class distance():
+    def __init__(self):
+        # GPIO Mode (BOARD / BCM)
+        GPIO.setmode(GPIO.BCM)
 
-# set GPIO Pins
-GPIO_TRIGGER = 18
-GPIO_ECHO = 24
+        # set GPIO Pins
+        GPIO_TRIGGER = 18
+        GPIO_ECHO = 24
 
-# set GPIO direction (IN / OUT)
-GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
-GPIO.setup(GPIO_ECHO, GPIO.IN)
+        # set GPIO direction (IN / OUT)
+        GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
+        GPIO.setup(GPIO_ECHO, GPIO.IN)
 
+        self.dist = 0
+        self.on= False
 
-def distance():
-    while True:
-        #delay to improve threading, removes stress from cpu
-        time.delay(0.1)
-        # set Trigger to HIGH
-        GPIO.output(GPIO_TRIGGER, True)
-        # set Trigger after 0.01ms to LOW
-        time.sleep(0.00001)
-        GPIO.output(GPIO_TRIGGER, False)
+    def start(self):
+        Thread(target = self.run,args=()).start()
+        return self
 
-        StartTime = time.time()
-        StopTime = time.time()
+    def run(self):
+        self.on = True
+        while self.on:
+            #delay to improve threading, removes stress from cpu
+            time.delay(0.1)
+            # set Trigger to HIGH
+            GPIO.output(GPIO_TRIGGER, True)
+            # set Trigger after 0.01ms to LOW
+            time.sleep(0.00001)
+            GPIO.output(GPIO_TRIGGER, False)
 
-        # save StartTime
-        while GPIO.input(GPIO_ECHO) == 0:
             StartTime = time.time()
-
-        # save time of arrival
-        while GPIO.input(GPIO_ECHO) == 1:
             StopTime = time.time()
 
-        # time difference between start and arrival
-        TimeElapsed = StopTime - StartTime
-        # multiply with the sonic speed (34300 cm/s)
-        # and divide by 2, because there and back
-        distance = (TimeElapsed * 34300) / 2
+            # save StartTime
+            while GPIO.input(GPIO_ECHO) == 0:
+                StartTime = time.time()
 
-        return distance
+            # save time of arrival
+            while GPIO.input(GPIO_ECHO) == 1:
+                StopTime = time.time()
 
-# if __name__ == '__main__':
-#     try:
-#         while True:
-#             dist = distance()
-#             print ("Measured Distance = %.1f cm" % dist)
-#             time.sleep(1)
+            # time difference between start and arrival
+            TimeElapsed = StopTime - StartTime
+            # multiply with the sonic speed (34300 cm/s)
+            # and divide by 2, because there and back
+            self.dist= (TimeElapsed * 34300) / 2
+
+        def stop(self):
+            self.on = False
+            GPIO.cleanup()
+
+if __name__ == '__main__':
+     distanceThread = distance()
+     distanceThread.start()
+     try:
+         while True:
+             print ("Measured Distance = %.1f cm" % distanceThread.dist)
+             time.sleep(1)
 #
 #         # Reset by pressing CTRL + C
-#     except KeyboardInterrupt:
-#         print("Measurement stopped by User")
-#         GPIO.cleanup()
+     except KeyboardInterrupt:
+         print("Measurement stopped by User")
--- roboticleague/main.py
+++ roboticleague/main.py
@@ -1,15 +1,28 @@
-import RPI.GPIO as GPIO
-import time
-from /color_detectiion/colorprint/ import scanColor
-from /line detection/using_cv_demo1/ import line_follow, no_line
+from colorDetection.colorprint import scanColor
+from lineDetection.using_cv_demo1 import line_follow, no_line
 from crossing import distance
-GPIO.setmode(GPIO.BCM)
+from cameraInput import camera
 
 from threading import Thread
+from time import sleep 
 
+import RPI.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
+
+#init threads
+cameraThread = camera()
 colorThread = scanColor()
 lineThread = line_follow()
 distanceThread = distance()
+
+# Setup Output Pins
+# Left Forward
+GPIO.setup("P8_10", GPIO.OUT)
+# Right Forward
+GPIO.setup("P9_11", GPIO.OUT)
+# start the motors
+GPIO.output("P8_10", GPIO.HIGH)
+GPIO.output("P9_11", GPIO.HIGH)
 
 motorsleft=1
 motorsright=2
@@ -26,16 +39,12 @@ def stop(stoptime):
     time.sleep(stoptime)
     line_follow()
 
+#ig we will have this function control the car
 def maiin():
-
-    #####################################needs threading here#################################
-    #line_follow()
-    #colour = scanColor()
-    #dist = distance()
-
-    colorThread.start()
-    lineThread.start()
-    distanceThread.start()
+    color = colorThread.color
+    dist = distanceThread.dist
+    no_line = lineThread.no_line
+    cx = lineThread.cx
 
     if dist < 5:
         GPIO.output(motorsleft, False)
@@ -47,17 +56,43 @@ def maiin():
     if colour == B:
         stop(2000)
 
-    if no_line == True:
+    if no_line != True:
+        if cx >= 120:
+            GPIO.output("P8_10", GPIO.HIGH)
+            GPIO.output("P9_11", GPIO.LOW)
+        if cx < 120 and cx > 50:
+            GPIO.output("P8_10", GPIO.LOW)
+            GPIO.output("P9_11", GPIO.LOW)
+        if cx <= 50:
+            GPIO.output("P8_10", GPIO.LOW)
+            GPIO.output("P9_11", GPIO.HIGH)
+
+    else:
         forward()
         time.sleep(1000)#enough time for line cut 15cm dist
         line_follow()
-          
+
+
+
+    #print(f"distance={dist}")
+    #print(f"color={colorThread.color}")
+    #print(cameraThread.frame)
+    #print(f"frame={cameraThread.frame}")
+    #print(f"line={line}")
 
 
 if __name__ == '__main__':
-    webcam = cv2.VideoCapture(0)
+    distanceThread.start()
+    cameraThread.start()
+    colorThread.start()
+    lineThread.start()
+
     while (True):
         maiin()
+        colorThread.frame = cameraThread.frame 
+        lineThread.frame = cameraThread.frame
+        dist = distanceThread.dist
+        sleep(2)
 
 """
 for when task is done
@@ -65,6 +100,6 @@ for when task is done
 colorThread.stop()
 lineThread.stop()
 distanceThread.stop()
-
+cameraThread.stop()
 
 """
