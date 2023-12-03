4b90b21cae97f80c2d4d02c5fdacd16348244a88
SotchNam <sotchnam@gmail.com>
2023-12-03 23:43:52 +0100

    fixed more color issues

--- colorDetection/colorprint.py
+++ colorDetection/colorprint.py
@@ -26,40 +26,38 @@ class scanColor(Thread):
         return self
 
     def run(self):
+        #color ranges: hsv (180,255,255)max
+        #red has a part in beginning and the end of hue spectrum so two ranges are needed
+        #use HSV_Treshhold.py to get calibrated values
+        red_lower1 = np.array([0, 87, 111], np.uint8)
+        red_upper1 = np.array([25, 255, 255], np.uint8) 
+
+        red_lower = np.array([160, 87, 111], np.uint8)
+        red_upper = np.array([180, 255, 255], np.uint8) 
+
+        green_lower = np.array([40, 52, 72], np.uint8)
+        green_upper = np.array([92, 255, 255], np.uint8)
+
+        blue_lower = np.array([94, 70, 82], np.uint8)
+        blue_upper = np.array([121, 255, 255], np.uint8)
+
         self.on = True
         while self.on:
             #delay to improve threading, removes stress from cpu
             time.sleep(1.1)
             try:
                 #getting smaller picture
-
                 y= self.frame.shape[0]
                 x= self.frame.shape[1]
                 imageFrame = self.frame[int(y*y1):int(y*y2), int(x*x1):int(x*x2)]
                 #imageFrame = self.frame[y1:y2,x1:x2]
                 #bgr to hsv
                 hsvFrame = cv2.cvtColor(imageFrame, cv2.COLOR_BGR2HSV)
-            #getting avg color from frame, might consider using dominant color
+                #getting avg color from frame, might consider using dominant color
                 avg_color_per_row = np.average(hsvFrame, axis=0) 
                 avg_color = np.average(avg_color_per_row, axis=0)
 
-            #color ranges: hsv (180,255,255)max
-            #red has a part in beginning and the end of hue spectrum so two ranges are needed
-            #use HSV_Treshhold.py to get calibrated values
-                red_lower1 = np.array([0, 87, 111], np.uint8)
-                red_upper1 = np.array([25, 255, 255], np.uint8) 
-
-                red_lower = np.array([156, 87, 111], np.uint8)
-                red_upper = np.array([180, 255, 255], np.uint8) 
-
-                green_lower = np.array([30, 52, 72], np.uint8)
-                green_upper = np.array([92, 255, 255], np.uint8)
-
-                blue_lower = np.array([94, 80, 2], np.uint8)
-                blue_upper = np.array([120, 255, 255], np.uint8)
-
-            #test the avg color with each color range
-
+                #test the avg color with each color range
                 #print(self.frame)
                 if  ((np.less_equal(avg_color,red_upper1)).all() and (np.greater_equal(avg_color,red_lower1)).all()) or ((np.less_equal(avg_color,red_upper)).all() and (np.greater_equal(avg_color,red_lower)).all()):
                         self.color= 'R'
@@ -84,7 +82,9 @@ if __name__ == '__main__':
     colorThread.frame = imageFrame
     colorThread.start()
     while(True):
+        _, imageFrame = webcam.read()
         colorThread.frame = imageFrame
+        cv2.imshow("Frame", imageFrame) 
         print(colorThread.color)
         if cv2.waitKey(1) & 0xFF == ord('q'):
                 colorThread.stop()
